24952dc565277cad723a12dbcf2c662bd970bbbc
vagrant <vagrant@vagrant.vm>
2020-10-07 03:37:20 +0000

    started new if list

--- markov.py
+++ markov.py
@@ -50,21 +50,32 @@ def make_chains(text_string):
 
     # your code goes here
     markov_words = text_string.split()
-    markov_list = []
+    # markov_list = []
     for i in range(len(markov_words) - 2 ):
         # markov_list.extend(markov_words[i], markov_words[i+1])
         markov_tuples = tuple((markov_words[i], markov_words[i+1]))
         # markov_list = markov_list + markov_tuples
-        markov_list.append(markov_tuples)
+        # markov_list.append(markov_tuples)
+        word_list = markov_words[i + 2]            
+            
+        if tuple_key not in chains:
+            chains[tuple_key] = []
+        chains[tuple_key].append(word_list)
+            
+#  for i in range(len(words) - 2):
+#         key = (words[i], words[i + 1])
+#         value = words[i + 2]
 
-        for tuple_key in markov_list:
+#         if key not in chains:
+#             chains[key] = []
+
+#         chains[key].append(value)
+
+
             
-            if tuple_key not in chains:
-                chains[tuple_key] = list([chains.get(tuple_key, markov_words[i + 2])])
-            
-            for tuple_key, value in chains:
-                (list(value).append(markov_words[i]))
-            # 
+#             for tuple_key, value in chains:
+#                 (list(value).append(markov_words[i]))
+#             # 
             #     chains[tuple_key] = [chains[tuple_key] + (markov_words[i + 2])]
             # if tuple_key in chains:
             #     chains[tuple_key] = add to dict(chains.get(tuple_key, markov_words[i + 2])
